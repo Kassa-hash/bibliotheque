6963624ca60c78e0f22ee6f01606b9520a6fe3ba
Kassa-hash <kassa6173@gmail.com>
2025-07-02 22:18:49 +0300

    preter livre

--- demo(1)/demo/src/main/java/racine/test/exemplaire/ExemplaireRepository.java
+++ demo(1)/demo/src/main/java/racine/test/exemplaire/ExemplaireRepository.java
@@ -1,6 +1,11 @@
 package racine.test.exemplaire;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.jpa.repository.Query;
+import java.util.Optional;
 
 public interface ExemplaireRepository extends JpaRepository<Exemplaire,Long> {
+    @Query("SELECT e FROM Exemplaire e WHERE e.livre.id = :livreId AND e.numero = :numero")
+    Optional<Exemplaire> findByLivreIdAndNumero(@Param("livreId") Long livre_id, @Param("numero") Integer numero);
 }
