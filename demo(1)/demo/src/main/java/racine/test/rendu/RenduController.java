08980d52c287e80845c2f6707dd4f5c8449032dd
Kassa-hash <kassa6173@gmail.com>
2025-07-03 22:11:23 +0300

    rendre un livre

--- demo(1)/demo/src/main/java/racine/test/rendu/RenduController.java
+++ demo(1)/demo/src/main/java/racine/test/rendu/RenduController.java
@@ -5,9 +5,14 @@ import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import racine.test.penalite.Penalite;
+import racine.test.penalite.PenaliteService;
 import racine.test.pret.Pret;
 import racine.test.pret.PretRepository;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.Date;
 
 @Controller
@@ -16,11 +21,13 @@ public class RenduController {
 
     private final RenduService renduService;
     private final PretRepository pretRepository;
+    private final PenaliteService penaliteService;
 
     @Autowired
-    public RenduController(RenduService renduService, PretRepository pretRepository) {
+    public RenduController(RenduService renduService, PretRepository pretRepository, PenaliteService penaliteService) {
         this.renduService = renduService;
         this.pretRepository = pretRepository;
+        this.penaliteService = penaliteService;
     }
 
     @GetMapping("/retour/{id}")
@@ -38,8 +45,18 @@ public class RenduController {
 
         Rendu rendu = new Rendu();
         rendu.setPret(pret);
-        rendu.setDateRendu(dateRendu);
+        rendu.setDateRendu((dateRendu));
 
+        LocalDate daterendu2 = dateRendu.toInstant()
+                .atZone(ZoneId.systemDefault())
+                .toLocalDate();
+
+        if (dateRendu.after(pret.getDateLimite()))
+        {
+            Penalite penalite=new Penalite();
+            penalite.setDateFin(daterendu2.plusDays(14));
+            penaliteService.createPenalite(penalite);
+        }
         renduService.createRendu(rendu);
 
         redirectAttributes.addFlashAttribute("success", "Rendu enregistré avec succès pour le prêt #" + idPret);
