31adb29811f1499be76790ab56d81189fb65c5e3
Kassa-hash <kassa6173@gmail.com>
2025-07-03 16:53:20 +0300

    afficher les reservations

--- demo(1)/demo/src/main/java/racine/test/reservation/Reservation.java
+++ demo(1)/demo/src/main/java/racine/test/reservation/Reservation.java
@@ -1,4 +1,69 @@
 package racine.test.reservation;
 
+import jakarta.persistence.*;
+import racine.test.adherent.Adherent;
+import racine.test.exemplaire.Exemplaire;
+
+import java.time.LocalDate;
+
+@Entity
 public class Reservation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "idAdherent", nullable = false)
+    private Adherent adherent;
+
+    @ManyToOne
+    @JoinColumn(name = "idExemplaire", nullable = false)
+    private Exemplaire exemplaire;
+
+    @Column(name = "dateDemande", nullable = false)
+    private LocalDate dateDemande;
+
+    @Column(name = "datePret", nullable = false)
+    private LocalDate datePret;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Adherent getAdherent() {
+        return adherent;
+    }
+
+    public void setAdherent(Adherent adherent) {
+        this.adherent = adherent;
+    }
+
+    public Exemplaire getExemplaire() {
+        return exemplaire;
+    }
+
+    public void setExemplaire(Exemplaire exemplaire) {
+        this.exemplaire = exemplaire;
+    }
+
+    public LocalDate getDateDemande() {
+        return dateDemande;
+    }
+
+    public void setDateDemande(LocalDate dateDemande) {
+        this.dateDemande = dateDemande;
+    }
+
+    public LocalDate getDatePret() {
+        return datePret;
+    }
+
+    public void setDatePret(LocalDate datePret) {
+        this.datePret = datePret;
+    }
 }
--- demo(1)/demo/src/main/java/racine/test/reservation/ReservationRepository.java
+++ demo(1)/demo/src/main/java/racine/test/reservation/ReservationRepository.java
@@ -1,4 +1,9 @@
 package racine.test.reservation;
 
-public interface ReservationRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReservationRepository extends JpaRepository<Reservation, Long> {
 }
+
