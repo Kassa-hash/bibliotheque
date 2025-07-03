d0e85678621280c650db5cecc8e2a01da09ba26d
Kassa-hash <kassa6173@gmail.com>
2025-07-03 18:55:38 +0300

    prolonger un pret

--- demo(1)/demo/src/main/java/racine/test/prolongement/Prolongement.java
+++ demo(1)/demo/src/main/java/racine/test/prolongement/Prolongement.java
@@ -1,4 +1,46 @@
 package racine.test.prolongement;
 
+import jakarta.persistence.*;
+import racine.test.pret.Pret;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "prolongement")
 public class Prolongement {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "idPret", nullable = false)
+    private Pret pret;
+
+    @Column(name = "nouvelleDate", nullable = false)
+    private LocalDate nouvelleDate;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Pret getPret() {
+        return pret;
+    }
+
+    public void setPret(Pret pret) {
+        this.pret = pret;
+    }
+
+    public LocalDate getNouvelleDate() {
+        return nouvelleDate;
+    }
+
+    public void setNouvelleDate(LocalDate nouvelleDate) {
+        this.nouvelleDate = nouvelleDate;
+    }
 }
--- demo(1)/demo/src/main/java/racine/test/prolongement/ProlongementRepository.java
+++ demo(1)/demo/src/main/java/racine/test/prolongement/ProlongementRepository.java
@@ -1,4 +1,10 @@
 package racine.test.prolongement;
 
-public interface ProlongementRepository {
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProlongementRepository extends JpaRepository<Prolongement, Long> {
+
 }
