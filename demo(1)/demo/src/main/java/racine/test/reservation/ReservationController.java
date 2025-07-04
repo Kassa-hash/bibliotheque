970229d35fac2e740599346f3c15920141fb5a95
Kassa-hash <kassa6173@gmail.com>
2025-07-04 14:41:29 +0300

    ok

--- demo(1)/demo/src/main/java/racine/test/reservation/ReservationController.java
+++ demo(1)/demo/src/main/java/racine/test/reservation/ReservationController.java
@@ -3,14 +3,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import racine.test.adherent.Adherent;
 import racine.test.adherent.AdherentService;
 import racine.test.exemplaire.Exemplaire;
 import racine.test.exemplaire.ExemplaireService;
 import racine.test.livre.Livre;
 import racine.test.livre.LivreService;
+import racine.test.penalite.Penalite;
+import racine.test.penalite.PenaliteService;
 import racine.test.pret.Pret;
 import racine.test.pret.PretService;
+import racine.test.pret.TypePret;
+import racine.test.pret.TypePretService;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -21,8 +26,12 @@ public class ReservationController {
 
     @Autowired
     private ReservationService reservationService;
+
     @Autowired
     private PretService pretService;
+
+    @Autowired
+    private TypePretService typePretService;
 
     @Autowired
     private AdherentService adherentService;
@@ -33,10 +42,16 @@ public class ReservationController {
     @Autowired
     private ExemplaireService exemplaireService;
 
+    @Autowired
+    private PenaliteService penaliteService;
+
     @GetMapping("/reservation")
     public String getAllReservations(Model model) {
+
         List<Reservation> reservations=reservationService.getAllReservations();
+        List<TypePret> typePrets=typePretService.getAllTypePrets();
         model.addAttribute("reservations",reservations);
+        model.addAttribute("typePrets",typePrets);
         return  "reservation";
     }
 
@@ -55,7 +70,12 @@ public class ReservationController {
     public String submitReservation(@RequestParam("adherent") Long adherentId,
                                     @RequestParam("exemplaire") Long exemplaireId,
                                     @RequestParam("dateDemande") LocalDate dateDemande,
-                                    @RequestParam("datePret") LocalDate datePret) {
+                                    @RequestParam("datePret") LocalDate datePret,
+                                   RedirectAttributes redirectAttributes
+    )
+
+
+    {
 
         // Récupérer les entités avec gestion des Optional
         Optional<Adherent> adherentOpt = adherentService. getAdherentById(adherentId);
@@ -65,6 +85,17 @@ public class ReservationController {
         if (adherentOpt.isEmpty() || exemplaireOpt.isEmpty()) {
             // Gérer l'erreur - rediriger avec un message d'erreur
             return "redirect:/";
+        }
+
+        List<Penalite> penalites = penaliteService.getAllPenalites();
+
+        for (Penalite p : penalites) {
+            if (p.getAdherent().getId() == adherentId) {
+                if (LocalDate.now().isBefore(p.getDateFin())) {
+                    redirectAttributes.addFlashAttribute("errorMessage", "Adhérent encore pénalisé");
+                    return "redirect:/reservation";
+                }
+            }
         }
 
         Reservation reservation = new Reservation();
